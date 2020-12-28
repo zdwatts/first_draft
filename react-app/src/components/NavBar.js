88ad1863c63814a01f767f9158429ab03e711246
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-28 17:52:13 -0600

    move navbar css

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "./auth/LogoutButton";
+import "./navbar.css";
 
 const NavBar = ({ setAuthenticated }) => {
 	return (
