6ef22ebf204b7a60826ff5dc925d5bf175d61010
Saad <saad_shaikh@yahoo.com>
2020-12-28 17:08:44 -0500

    made a first req to backend, woohoo!

--- react-app/src/components/Story.js
+++ react-app/src/components/Story.js
@@ -2,21 +2,25 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Story() {
-  const [story, setStory] = useState([]);
+	const [story, setStory] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      const data = await axios.get("http://127.0.0.1:5000/api/stories");
-      console.log(data);
-      console.log("alksjdf");
-      setStory(data);
-    })();
-    console.log("hi");
-  }, [story]);
+	useEffect(() => {
+		(async () => {
+			const data = await axios.get("http://127.0.0.1:5000/api/stories");
+			console.log(data.data.stories[0]);
+			setStory(data.data.stories[0]);
+		})();
+	}, []);
 
-  console.log(story);
+	console.log(story);
 
-  return <h1>Story Component</h1>;
+	return (
+		<div>
+			<h1>{story.author_id}</h1>
+			<h3>{story.title}</h3>
+			<h3>{story.body}</h3>
+		</div>
+	);
 }
 
 export default Story;
