d4d416be973e936528493fcbe5db8e1ac6fadbdf
Saad <saad_shaikh@yahoo.com>
2020-12-28 15:46:22 -0500

    created story table;

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,46 +9,57 @@ import User from "./components/User";
 import { authenticate } from "./services/auth";
 
 function App() {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [loaded, setLoaded] = useState(false);
+	const [authenticated, setAuthenticated] = useState(false);
+	const [loaded, setLoaded] = useState(false);
 
-  useEffect(() => {
-    (async() => {
-      const user = await authenticate();
-      if (!user.errors) {
-        setAuthenticated(true);
-      }
-      setLoaded(true);
-    })();
-  }, []);
+	useEffect(() => {
+		(async () => {
+			const user = await authenticate();
+			if (!user.errors) {
+				setAuthenticated(true);
+			}
+			setLoaded(true);
+		})();
+	}, []);
 
-  if (!loaded) {
-    return null;
-  }
+	if (!loaded) {
+		return null;
+	}
 
-  return (
-    <BrowserRouter>
-      <NavBar setAuthenticated={setAuthenticated} />
-      <Route path="/login" exact={true}>
-        <LoginForm
-          authenticated={authenticated}
-          setAuthenticated={setAuthenticated}
-        />
-      </Route>
-      <Route path="/sign-up" exact={true}>
-        <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
-      </Route>
-      <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
-        <UsersList/>
-      </ProtectedRoute>
-      <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
-        <User />
-      </ProtectedRoute>
-      <ProtectedRoute path="/" exact={true} authenticated={authenticated}>
-        <h1>My Home Page</h1>
-      </ProtectedRoute>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<NavBar setAuthenticated={setAuthenticated} />
+			<Route path="/login" exact={true}>
+				<LoginForm
+					authenticated={authenticated}
+					setAuthenticated={setAuthenticated}
+				/>
+			</Route>
+			<Route path="/sign-up" exact={true}>
+				<SignUpForm
+					authenticated={authenticated}
+					setAuthenticated={setAuthenticated}
+				/>
+			</Route>
+			<ProtectedRoute
+				path="/users"
+				exact={true}
+				authenticated={authenticated}
+			>
+				<UsersList />
+			</ProtectedRoute>
+			<ProtectedRoute
+				path="/users/:userId"
+				exact={true}
+				authenticated={authenticated}
+			>
+				<User />
+			</ProtectedRoute>
+			<ProtectedRoute path="/" exact={true} authenticated={authenticated}>
+				<h1>My Home Page</h1>
+			</ProtectedRoute>
+		</BrowserRouter>
+	);
 }
 
 export default App;
