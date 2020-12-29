9d10d0b7e6edc529325abaea1c6667caba5d8ee2
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-28 21:05:15 -0600

    add text to logged out header

--- react-app/src/components/DefaultHeader.js
+++ react-app/src/components/DefaultHeader.js
@@ -1,6 +1,18 @@
 import React from "react";
 const DefaultHome = ({ setAuthenticated, authenticated }) => {
-    return authenticated ? <h1>Logged in</h1> : <h1>Logged out</h1>;
+    return authenticated ? (
+      <h1>Logged in</h1>
+    ) : (
+      <div className="Logged_out_header">
+        <div>
+          <h1>Explore new perspectives</h1>
+          <p>
+            Read and share ideas from independent voices, world-class
+            publications, and experts from around the globe. Everyone's welcome.
+          </p>
+        </div>
+      </div>
+    );
 };
 
 export default DefaultHome;
