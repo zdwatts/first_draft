681cd4dec106294ef1d43cbedcbac66ccfb7bc31
zacwatts <zdwatts@gmail.com>
2020-12-29 11:11:02 -0700

    wrapped form in div

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -4,64 +4,66 @@ import { login } from "../../services/auth";
 import "./LoginForm.css";
 
 const LoginForm = ({ authenticated, setAuthenticated }) => {
-  const [errors, setErrors] = useState([]);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const [errors, setErrors] = useState([]);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  const onLogin = async (e) => {
-    e.preventDefault();
-    const user = await login(email, password);
-    if (!user.errors) {
-      setAuthenticated(true);
-    } else {
-      setErrors(user.errors);
-    }
-  };
+	const onLogin = async (e) => {
+		e.preventDefault();
+		const user = await login(email, password);
+		if (!user.errors) {
+			setAuthenticated(true);
+		} else {
+			setErrors(user.errors);
+		}
+	};
 
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
+	const updateEmail = (e) => {
+		setEmail(e.target.value);
+	};
 
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
+	const updatePassword = (e) => {
+		setPassword(e.target.value);
+	};
 
-  if (authenticated) {
-    return <Redirect to="/" />;
-  }
+	if (authenticated) {
+		return <Redirect to="/" />;
+	}
 
-  return (
-    <form onSubmit={onLogin} className="login-form">
-      <div>
-        {errors.map((error) => (
-          <div>{error}</div>
-        ))}
-      </div>
-      <div>
-        <input
-          className="input"
-          name="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <input
-          className="input"
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={updatePassword}
-        />
-      </div>
-      <button type="submit" className="login-button">
-        Login
-      </button>
-    </form>
-  );
+	return (
+		<div className="login-form-div">
+			<form onSubmit={onLogin} className="login-form">
+				<div>
+					{errors.map((error) => (
+						<div>{error}</div>
+					))}
+				</div>
+				<div>
+					<input
+						className="input"
+						name="email"
+						type="text"
+						placeholder="Email"
+						value={email}
+						onChange={updateEmail}
+					/>
+				</div>
+				<div>
+					<input
+						className="input"
+						name="password"
+						type="password"
+						placeholder="Password"
+						value={password}
+						onChange={updatePassword}
+					/>
+				</div>
+				<button type="submit" className="login-button">
+					Login
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default LoginForm;
