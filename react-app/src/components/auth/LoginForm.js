e1a135ffeac856574df65be8fb8570918c7a5490
zacwatts <zdwatts@gmail.com>
2020-12-29 11:25:31 -0700

    got background working

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -31,7 +31,7 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
 	}
 
 	return (
-		<div className="login-form-div">
+		<div className="login-form-div h-100vh pattern-vertical-lines-md yellow-light bg-yellow flex justify-center items-center bg-fixed">
 			<form onSubmit={onLogin} className="login-form">
 				<div>
 					{errors.map((error) => (
