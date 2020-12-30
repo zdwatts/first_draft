ef797f3e5a1fabedab5cc57310cc033124dd30f8
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-29 20:37:25 -0600

    fix footer spacing

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -7,8 +7,7 @@ import "./Footer.css";
 const Footer = ({ setAuthenticated, authenticated }) => {
   return (
     <nav className="footer">
-      <div className="main-logo">
-        <h1 className="logo">{"</>"}</h1>
+      <div className="main-logo-footer">
         <h1 className="logo-text">Medium</h1>
       </div>
       <ul className="nav_links">
@@ -19,6 +18,7 @@ const Footer = ({ setAuthenticated, authenticated }) => {
             exact={true}
             activeClassName="active"
           >
+            <i class="fas fa-home"></i>{" "}
             Home
           </NavLink>
           <a
@@ -27,7 +27,7 @@ const Footer = ({ setAuthenticated, authenticated }) => {
             exact={true}
             activeClassName="active"
           >
-            GitHub
+            <i class="fab fa-github footer-icon fa-md"></i> GitHub
           </a>
         </li>
       </ul>
