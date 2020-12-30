daef18a4c07695213431a0e470b29f86856d1d1c
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-29 19:35:40 -0600

    fixed the input

--- react-app/src/components/Page/CreateStory.js
+++ react-app/src/components/Page/CreateStory.js
@@ -55,7 +55,7 @@ function CreateStory({ authenticate }) {
 			<form onSubmit={handleSubmit}>
 				<div className="input-div">
 				<label className="story-title-label">Story Title</label>
-				<input className="title-input" type="text" placeholder="title" onChange={titleChange} />
+				<input className="title-input" type="text" placeholder='"Why Sequelize Sucks"' onChange={titleChange} />
 				</div>
 				<Editor
 					apiKey={apiKey}
