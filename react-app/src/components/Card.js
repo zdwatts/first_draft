7a5bb59182a37ddfa2866b346f5d006cb41832f0
zacwatts <zdwatts@gmail.com>
2020-12-29 19:59:48 -0700

    styled card titles and added links to story

--- react-app/src/components/Card.js
+++ react-app/src/components/Card.js
@@ -7,7 +7,11 @@ const Card = ({ story, idx }) => {
 	return (
 		<div className="card">
 			<h1 className="card-number">0{idx + 1}</h1>
-			<h2 className="card-title">{story.title}</h2>
+			<h2>
+				<a className="card-title" href={`/stories/${story.id}`}>
+					{story.title}
+				</a>
+			</h2>
 		</div>
 	);
 };
