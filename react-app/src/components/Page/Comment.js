6f19416b53f656e896ae4f12618d66060c2b8164
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-31 13:43:43 -0600

    fix comment typo

--- react-app/src/components/Page/Comment.js
+++ react-app/src/components/Page/Comment.js
@@ -4,9 +4,9 @@ import "./Story.css"
 
 function Comment({ comments, currentUser }) {
 	return (
-		<div class="comments-container">
+		<div className="comments-container">
 			{comments.map((comment, i) => (
-				<div key={i}>
+				<div claskey={i}>
 					<p>{comment.user} commented:</p>
 					<div>{parse(comment.comment)}</div>
 				</div>
