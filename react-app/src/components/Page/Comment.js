d29cca7fea7cf000a790f96b9904922b7786bb34
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-31 13:40:26 -0600

    fix comment bo width

--- react-app/src/components/Page/Comment.js
+++ react-app/src/components/Page/Comment.js
@@ -1,9 +1,10 @@
 import React from "react";
 import parse from "html-react-parser";
+import "./Story.css"
 
 function Comment({ comments, currentUser }) {
 	return (
-		<div>
+		<div class="comments-container">
 			{comments.map((comment, i) => (
 				<div key={i}>
 					<p>{comment.user} commented:</p>
