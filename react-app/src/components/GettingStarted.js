7ff6f292287b1ca366d9d75b49fe9ec0bcc61298
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-31 16:36:52 -0600

    duplicate cardds

--- react-app/src/components/GettingStarted.js
+++ react-app/src/components/GettingStarted.js
@@ -7,12 +7,23 @@ const GettingStarted = ({ authenticated }) => {
   ) : (
     <div className="getting-started">
       <h1 className="start-title">Getting Started</h1>
-      <h1 className="feature-title">Create an account.</h1>
-      <div class="pattern-diagonal-lines-sm gray-lighter bg-dots animate__animated animate__slideInLeft">
-        <img
-          class="sign-image animate__animated animate__slideInRight"
-          src="signup.gif"
-        />
+      <div>
+        <h1 className="feature-title">Create an account.</h1>
+        <div class="pattern-diagonal-lines-sm gray-lighter bg-dots">
+          <img class="sign-image" src="signup.gif" />
+        </div>
+      </div>
+      <div>
+        <h1 className="feature-title">Create an account.</h1>
+        <div class="pattern-diagonal-lines-sm gray-lighter bg-dots">
+          <img class="sign-image" src="signup.gif" />
+        </div>
+      </div>
+      <div>
+        <h1 className="feature-title">Create an account.</h1>
+        <div class="pattern-diagonal-lines-sm gray-lighter bg-dots">
+          <img class="sign-image" src="signup.gif" />
+        </div>
       </div>
     </div>
   );
