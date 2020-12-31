287637b81293c4ae34758c9dbb17a5ea5c391774
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-31 16:54:14 -0600

    writing stores

--- react-app/src/components/GettingStarted.js
+++ react-app/src/components/GettingStarted.js
@@ -7,22 +7,33 @@ const GettingStarted = ({ authenticated }) => {
   ) : (
     <div className="getting-started">
       <h1 className="start-title">Getting Started</h1>
-      <div>
-        <h1 className="feature-title">Create an account.</h1>
-        <div class="pattern-diagonal-lines-sm gray-lighter bg-dots">
-          <img class="sign-image" src="signup.gif" />
+      <div className="card-wrapper-2">
+        <div className="card-1">
+          <div>
+            <h1 className="feature-title">Create an account.</h1>
+            <p>Sign up today to begin sharing your stries with others.</p>
+          </div>
+          <div class="pattern-diagonal-lines-sm gray-lighter bg-dots">
+            <img class="sign-image" src="signup.gif" />
+          </div>
         </div>
-      </div>
-      <div>
-        <h1 className="feature-title">Create an account.</h1>
-        <div class="pattern-diagonal-lines-sm gray-lighter bg-dots">
-          <img class="sign-image" src="signup.gif" />
+        <div className="card-2">
+          <div>
+            <h1 className="feature-title">Create an account.</h1>
+            <p>Sign up today to begin sharing your stries with others.</p>
+          </div>
+          <div class="pattern-diagonal-lines-sm gray-lighter bg-dots">
+            <img class="sign-image" src="signup.gif" />
+          </div>
         </div>
-      </div>
-      <div>
-        <h1 className="feature-title">Create an account.</h1>
-        <div class="pattern-diagonal-lines-sm gray-lighter bg-dots">
-          <img class="sign-image" src="signup.gif" />
+        <div className="card-3">
+          <div>
+            <h1 className="feature-title">Create an account.</h1>
+            <p>Sign up today to begin sharing your stries with others.</p>
+          </div>
+          <div class="pattern-diagonal-lines-sm gray-lighter bg-dots">
+            <img class="sign-image" src="signup.gif" />
+          </div>
         </div>
       </div>
     </div>
