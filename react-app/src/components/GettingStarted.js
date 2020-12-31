41a89f4fd8b333ac7e6b54b18f8ce206b508ef83
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-31 16:26:49 -0600

    creating real hover

--- react-app/src/components/GettingStarted.js
+++ react-app/src/components/GettingStarted.js
@@ -6,7 +6,11 @@ const GettingStarted = ({ authenticated }) => {
     <div></div>
   ) : (
     <div className="getting-started">
-        <h1 className="start-title">Getting Started</h1>
+      <h1 className="start-title">Getting Started</h1>
+      <h1 className="feature-title">Create an account.</h1>
+      <div class="pattern-diagonal-lines-sm gray-lighter bg-dots">
+        <img class="sign-image" src="signup.gif" />
+      </div>
     </div>
   );
 };
