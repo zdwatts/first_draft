5cfeeaf6d8d0ce39f07ccecf20b9036f554adc12
Saad <saad_shaikh@yahoo.com>
2021-01-02 18:31:08 -0500

    added modal for update story

--- react-app/src/components/Page/UpdateStory.js
+++ react-app/src/components/Page/UpdateStory.js
@@ -1,17 +1,45 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Editor } from "@tinymce/tinymce-react";
-import "./Story.css"
-import "./stories.css"
+import "./Story.css";
+import "./stories.css";
+import Modal from "react-modal";
+
+const customStyles = {
+	content: {
+		widht: "80%",
+		top: "50%",
+		left: "50%",
+		right: "auto",
+		bottom: "auto",
+		marginRight: "-50%",
+		transform: "translate(-50%, -50%)",
+	},
+};
 
 function UpdateStory({ id, title, body }) {
-	const [showForm, setShowForm] = useState(false);
+	const [showModal, setShowModal] = useState(false);
+
+	const closeModal = () => {
+		setShowModal(false);
+	};
+
 	return (
 		<div>
-			<button onClick={() => setShowForm(!showForm)} className="update-button">
+			<button
+				onClick={() => setShowModal(!showModal)}
+				className="update-button"
+			>
 				Update Story
 			</button>
-			{showForm ? <UpdateForm id={id} title={title} body={body} /> : ""}
+			<Modal
+				isOpen={showModal}
+				onRequestClose={closeModal}
+				style={customStyles}
+				contentLabel="EXAMPLE MODAL"
+			>
+				<UpdateForm id={id} title={title} body={body} />
+			</Modal>
 		</div>
 	);
 }
@@ -36,15 +64,29 @@ function UpdateForm({ id, title, body }) {
 
 	return (
 		<form onSubmit={handleSubmit}>
-			<input type="text" value={newTitle} onChange={handleChange} className="title-input"/>
+			<input
+				type="text"
+				value={newTitle}
+				onChange={handleChange}
+				className="title-input"
+			/>
 			<Editor
 				value={newBody}
 				apiKey={apiKey}
-				plugins="wordcount wordcount fullscreen emoticons image insertdatetime preview"
+				// plugins="wordcount wordcount fullscreen emoticons image insertdatetime preview"
+				init={{
+					height: 500,
+					width: 500,
+					plugins: [
+						"wordcount wordcount fullscreen emoticons image insertdatetime preview",
+					],
+				}}
 				onEditorChange={handleEditorChange}
 				className="animate__animated animate__hinge"
 			/>
-			<button type="submit" className="story-submit" >Confirm Changes</button>
+			<button type="submit" className="story-submit">
+				Confirm Changes
+			</button>
 		</form>
 	);
 }
