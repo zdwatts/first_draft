388c42bde6d028857e89e6798e20efabaa9c1632
zacwatts <zdwatts@gmail.com>
2021-02-03 15:50:57 -0700

    styled contact info in footer

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -11,103 +11,95 @@ const Footer = () => {
 		<footer className="footer">
 			<ul className="nav_links">
 				<li>
+					<p
+						style={{
+							fontSize: "1.5rem",
+						}}
+					>
+						Zac Watts
+					</p>
 					<a
 						href="https://www.linkedin.com/in/zac-watts-b27450106/"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						<FontAwesomeIcon
-							icon={faLinkedin}
-							size="2x"
-							style={{ color: "#15114A" }}
-						/>
+						<FontAwesomeIcon icon={faLinkedin} style={{ color: "#15114A" }} />
 					</a>
-					<p>Zac Watts</p>
 					<a
 						href="https://github.com/zdwatts"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						<FontAwesomeIcon
-							icon={faGithub}
-							size="2x"
-							style={{ color: "#15114A" }}
-						/>
+						<FontAwesomeIcon icon={faGithub} style={{ color: "#15114A" }} />
 					</a>
 				</li>
 				<li>
+					<p
+						style={{
+							fontSize: "1.5rem",
+						}}
+					>
+						Miguel Coria
+					</p>
 					<a
 						href="https://www.linkedin.com/in/miguel-coria-273888130/"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						<FontAwesomeIcon
-							icon={faLinkedin}
-							size="2x"
-							style={{ color: "#15114A" }}
-						/>
+						<FontAwesomeIcon icon={faLinkedin} style={{ color: "#15114A" }} />
 					</a>
-					<p>Miguel Coria</p>
 					<a
 						href="https://github.com/miguelcoria94"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						<FontAwesomeIcon
-							icon={faGithub}
-							size="2x"
-							style={{ color: "#15114A" }}
-						/>
+						<FontAwesomeIcon icon={faGithub} style={{ color: "#15114A" }} />
 					</a>
 				</li>
 				<li>
+					<p
+						style={{
+							fontSize: "1.5rem",
+						}}
+					>
+						Ronald Regan Palisuan
+					</p>
 					<a
 						href="https://www.linkedin.com/in/ronald-regan-palisuan-833451163/"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						<FontAwesomeIcon
-							icon={faLinkedin}
-							size="2x"
-							style={{ color: "#15114A" }}
-						/>
+						<FontAwesomeIcon icon={faLinkedin} style={{ color: "#15114A" }} />
 					</a>
-					<p>Ronald Regan Palisuan</p>
 					<a
 						href="https://github.com/zolvo"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						<FontAwesomeIcon
-							icon={faGithub}
-							size="2x"
-							style={{ color: "#15114A" }}
-						/>
+						<FontAwesomeIcon icon={faGithub} style={{ color: "#15114A" }} />
 					</a>
 				</li>
 				<li>
+					<p
+						style={{
+							fontSize: "1.5rem",
+						}}
+					>
+						Saad Shaikh
+					</p>
 					<a
 						href="https://www.linkedin.com/in/saadshaikh18/"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						<FontAwesomeIcon
-							icon={faLinkedin}
-							size="2x"
-							style={{ color: "#15114A" }}
-						/>
+						<FontAwesomeIcon icon={faLinkedin} style={{ color: "#15114A" }} />
 					</a>
-					<p>Saad Shaikh</p>
 					<a
 						href="https://github.com/saadjs"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						<FontAwesomeIcon
-							icon={faGithub}
-							size="2x"
-							style={{ color: "#15114A" }}
-						/>
+						<FontAwesomeIcon icon={faGithub} style={{ color: "#15114A" }} />
 					</a>
 				</li>
 			</ul>
